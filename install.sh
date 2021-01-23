49ebeaf8d4601c802e47c1e69e7659a80166311d
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-01-23 08:48:30 -0800

    Fixes bug in install.sh where alternate install locations were not being considered.

--- install.sh
+++ install.sh
@@ -4,12 +4,10 @@
 
 PATHS=($HOME/bin $HOME/.local/bin /usr/local/bin)
 
-for INSTALL_DIR in $PATHS; do
-  if [[ ":$PATH:" == *":INSTALL_DIR:"* ]]; then
+for INSTALL_DIR in "${PATHS[@]}"; do
+  if [[ $PATH  =~ (^|:)$INSTALL_DIR(:|$) ]]; then
     break
   fi
 done
-
-# echo $INSTALL_DIR
 
 ln -s $(pwd)/setup.sh $INSTALL_DIR/git-setup
